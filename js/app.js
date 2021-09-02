3cbd487937768dcdc408792445e687a5e8c35743
SethRatguy <sethowel@iu.edu>
2021-09-02 13:36:18 -0400

    changed thing for the lab

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var Red = 29
+var Green = 47
+var Blue = Red + (Green * 3)
+function setup() {
+    
+    createCanvas(800,600);
+    
+    background(90)
+    
+    rect(275, 175, 200, 50);
+
+    circle(200,200,100 );
+
+    circle(550,200, 100)
+
+
+    fill(Red, Green, Blue,)
+    circle(550,200, 50)
+
+    circle(200,200, 50)
+
+    
+}
