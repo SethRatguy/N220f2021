ea0b7b73d0e504ace834ce5ab17f6861723be2cc
SethRatguy <sethowel@iu.edu>
2021-10-04 13:11:08 -0400

    setting up the lab

--- Labs/lab5/lab1/app.js
+++ Labs/lab5/lab1/app.js
@@ -1,8 +1,13 @@
 let rp = 100
 
-let ds = document.createElement("div");
+let ds = document.getElementById("box")
+
 ds.style.backgroundColor = "#31D449"
 ds.style.width = rp + "px";
 ds.style.height = rp + "px";
+function size(){
+    ds.style.width = rp * 1.1 + "px";
+    ds.style.height = rp * 1.1 + "px";   
+}
 
 document.body.appendChild(ds)
