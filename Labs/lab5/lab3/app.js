74aad4ce909be1f3fb4009bb25e837d6cf8adb6a
SethRatguy <sethowel@iu.edu>
2021-10-04 13:23:52 -0400

    finish

--- Labs/lab5/lab3/app.js
+++ Labs/lab5/lab3/app.js
@@ -1,12 +1,18 @@
 
 
-let mo = document.createElement("div")
+let mo = document.getElementById("Hover")
 
-
+mo.style.backgroundColor = ("#000000")
 mo.style.width= "100px";
 mo.style.height= "100px";
 
+function black(){
+    mo.style.backgroundColor = ("#000000")
+}
 
+function blue(){
+    mo.style.backgroundColor  = ("#0D0DBD") 
+}
 document.body.appendChild(mo);
 
 
