71df63bbc475f632535d54bc37caf70b105e6b53
SethRatguy <sethowel@iu.edu>
2021-10-07 18:22:38 -0400

    change

--- Labs/lab5/lab1/app.js
+++ Labs/lab5/lab1/app.js
@@ -3,11 +3,13 @@ let rp = 100
 let ds = document.getElementById("box")
 
 ds.style.backgroundColor = "#31D449"
-ds.style.width = rp + "px";
-ds.style.height = rp + "px";
+ds.style.width = 100 + "px";
+ds.style.height = 100 + "px";
 function size(){
-    ds.style.width = rp * 1.1 + "px";
-    ds.style.height = rp * 1.1 + "px";   
+    rp = rp + (rp * .1);
+    ds.style.width = rp + "px";
+    ds.style.height = rp + "px"; 
+    console.log(rp)  
 }
 
 document.body.appendChild(ds)
