8a92ce4819badf3af323061bd3958b3590ab6b97
SethRatguy <sethowel@iu.edu>
2021-10-07 21:07:58 -0400

    updated looks

--- Labs/lab6/lab2/app.js
+++ Labs/lab6/lab2/app.js
@@ -7,7 +7,7 @@ let pet = {
 
 let Nam = document.getElementById("name")
 Nam.style.backgroundColor = "#8A7BE8"
-Nam.style.width = "50px"
+Nam.style.width = "100px"
 Nam.style.height = "50px"
 Nam.style.float = "left"
 Nam.innerHTML = "Name: " + pet.name
@@ -21,14 +21,14 @@ Ag.innerHTML = "Age: " +pet.age
 
 let Hap = document.getElementById("happiness")
 Hap.style.backgroundColor = "#8A7BE8"
-Hap.style.width = "50px"
+Hap.style.width = "100px"
 Hap.style.height = "50px"
 Hap.style.float = "left" 
 Hap.innerHTML = "Happiness: " + pet.happiness
 
 let Energ = document.getElementById("energy")
 Energ.style.backgroundColor = "#8A7BE8"
-Energ.style.width = "50px"
+Energ.style.width = "100px"
 Energ.style.height = "50px"
 Energ.style.float = "left"
 Energ.innerHTML = "Energy: " + pet.energy
@@ -39,11 +39,11 @@ Animal.style.width = "50px"
 Animal.style.height = "50px"
 
 function feed(){
-    Energ.innerHTML = pet.energy += 5
+    Energ.innerHTML = "Energy: " + (pet.energy += 5)
 }
 
 function play(){
-    Hap.innerHTML = pet.happiness += 5
+    Hap.innerHTML = "Happiness: " + (pet.happiness += 5)
 }
 
 
